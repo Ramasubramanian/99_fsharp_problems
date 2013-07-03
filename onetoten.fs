a1b178023a24b09f74090f34d1f8ed4040e3c283
Ramasubramanian <ram.6383@gmail.com>
2013-07-03 07:54:24 +0530

    Changed project structure and included 10-20 problem solutions

---
+++ onetoten.fs
@@ -0,0 +1,101 @@
+﻿namespace fsharpproblems
+module onetoten = 
+/// My solution to first 10 problems of 99 fsharp problems. Tried to use recursion 
+/// and pattern matching as far as possible and avoid in-built List utility functions
+/// Link - http://www.fssnip.net/an
+
+    /// 01-find the last element of a list.
+    let rec findLast list = 
+        match list with
+        | [] -> failwith "Empty List"
+        | [head] -> head
+        | head :: tail -> findLast tail
+
+
+    let rec findLastButN n list =
+        match n with 
+        | 0 -> findLast list
+        | n when n <= List.length list -> 
+            match list with
+            | [] -> failwith "Empty List"
+            | head :: tail -> if List.length tail = n then head else findLastButN n tail
+        | n -> failwith "Less elements in list!"
+
+    /// 02-find last but one element of the list.
+    let findLastButOne list = findLastButN 2 list
+
+
+    /// 03-find the element at specified index.
+    let rec elementAt n list =
+        match n with
+        | n when n <= List.length list -> 
+            match list with
+            | [] -> failwith "Empty list"
+            | head :: tail -> if n = 1 then head else elementAt (n - 1) tail 
+        | n -> failwith "Less elements in list!"
+
+    /// 04-find length of a specified list.
+    let rec len list =
+        match list with
+        | [] -> 0
+        | [x] -> 1
+        | head :: tail -> 1 + (len tail)
+
+    /// 05-reverse a list.
+    let reverse list = 
+        let rec reverse0 list seed = 
+            match list with 
+            | [] -> seed
+            | head :: tail -> reverse0 tail (head :: seed)
+        reverse0 list []
+
+    /// 06-check whether a given list is a palindrome.
+    let isPalindrome list = 
+        let mid = (len list) / 2
+        let rec reverseTake n list seed = 
+            match list with 
+            | [] -> (seed,[])
+            | head :: tail -> if n = 0 then (seed,head :: tail) else reverseTake (n - 1) tail (head :: seed)
+        let reversed, original = reverseTake mid list []
+        let result = 
+            match len reversed with
+            | n when n = len original -> reversed = original
+            | n ->
+                match original with  
+                | head :: tail -> reversed = tail
+                | _ -> false
+        result
+
+    /// 07-Flatten a nested list structure.
+    type 'a NestedList = List of 'a NestedList list | Elem of 'a
+
+    let flatten list =
+        let rec flatIt acc = function
+            | Elem x -> acc @ [x]
+            | List l -> List.fold (fun acc e -> flatIt acc e) acc l
+        flatIt [] list 
+
+    /// 08-Eliminate consecutive duplicates of list elements.
+    let compress list = 
+        let start = List.head list
+        let rec comp prev list acc =
+            match list with 
+            | [] -> acc
+            | first :: rest when first <> prev -> comp first rest (acc @ [first]) 
+            | first :: rest -> comp first rest acc
+        comp start (List.tail list) [start]   
+
+    /// 09-Pack consecutive duplicates of list elements into sublists.
+    let pack list = 
+        let start = List.head list
+        let rec pck prev list subacc mainacc =
+            match list with 
+            | [] -> mainacc @ [subacc]
+            | first :: rest when first = prev -> pck first rest (first :: subacc) mainacc  
+            | first :: rest -> pck first rest [first] (mainacc @ [subacc]) 
+        pck start (List.tail list) [start] []    
+
+    /// 10-Run-length encoding of a list.
+    let encode list = 
+        pack list |> List.map (fun l -> (List.length l,List.head l)) 
+
---
+++ tentotwenty.fs
@@ -0,0 +1,93 @@
+﻿namespace fsharpproblems
+
+open onetoten
+
+type 'a Encoding = Multiple of int * 'a | Single of 'a
+
+module tentotwenty =
+    ///11-Modified run-length encoding.
+    let modlen list = 
+        let gen l =
+            match List.length l with
+                | 1 -> Single(List.head l)
+                | n -> Multiple(n,List.head l)
+        pack list |> List.map gen
+    ///12-Decode a run-length encoded list.
+    let decode list =
+        let rec create v n acc =
+            match n with
+            | 0 -> acc
+            | x -> create v (x - 1) (v :: acc)
+        let generate = function
+            | Single(e) -> [e]
+            | Multiple(n,e) -> create e n []
+        List.fold (fun acc elem -> acc @ generate elem) [] list
+    /// alternative solution
+    let decode2 list =
+        let generate = function
+            | Single(e) -> [e]
+            | Multiple(n,e) -> List.replicate n e
+        List.fold (fun acc elem -> acc @ generate elem) [] list
+    ///13-Run-length encoding of a list (direct solution).
+    let rlencode list =
+        let create acc count e x = 
+            match count with 
+                | 1 -> (acc @ [Single(e)],1,x)
+                | n -> (acc @ [Multiple(n,e)],1,x)                
+        let encode (acc, count, e) elem = 
+            match elem with
+            | x when x = e -> (acc, count + 1, e)
+            | x -> create acc count e x                
+        let (acc, count, e) = List.fold encode ([], 1, List.head list) (List.tail list)
+        let (result, _, _) = create acc count e e
+        result
+    ///14-Duplicate the elements of a list.
+    let duplicate list =
+        List.foldBack (fun elem acc -> elem :: (elem :: acc)) list []
+    ///15-Replicate the elements of a list a given number of times.
+    let replicate v n =
+        let rec repl v n acc =
+            match n with
+            | 0 -> acc
+            | x -> repl v (x - 1) (v :: acc)
+        repl v n []
+    ///16-Drop every N'th element from a list.
+    let dropEvery list n =
+        let drop (acc, count) elem =
+            match count with
+            | x when x = n -> (acc, 1)
+            | x -> (acc @ [elem], count + 1)
+        let (acc,count) = List.fold drop ([],1) list 
+        acc
+    ///17-Split a list into two parts; the length of the first part is given.
+    let split list n =
+        let rec split0 acc list n =
+            match list with 
+            | [] -> (acc,[])
+            | l when n = 0 -> (acc, l)
+            | first :: rest -> split0 (acc @ [first]) rest (n - 1) 
+        split0 [] list n
+    ///18-Extract a slice from a list.
+    let slice list m n =
+        match n with
+        | 0 -> []
+        | n ->
+            let (temp, _) = split list n
+            let (_,result) = split temp (m - 1)
+            result 
+    ///19-Rotate a list N places to the left.        
+    let rotate list n =
+        let (a,b) = 
+            match n with 
+            | 0 -> (list,[])
+            | n when n > 0 -> split list n
+            | n -> split list (List.length list - (-n))
+        b @ a
+    ///20-Remove the K'th element from a list.
+    let removeAt n list =
+        let rec remove count acc rest =
+            match rest with
+            | [] -> failwith "Empty List!"
+            | first :: rest when count = n -> (first, acc @ rest)
+            | first :: rest -> remove (count + 1) (acc @ [first]) rest 
+        remove 0 [] list  
